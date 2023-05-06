8e0f052481a2bd9b135311a87e39fb80e0b9fce6
vadevid <kislitsun-van@mail.ru>
2023-05-06 20:28:10 +0300

    init

--- achiveserver/src/main/java/com/example/achiveserver/repository/CheckedRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.achiveserver.repository;
-
-import com.example.achiveserver.entity.Checked;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CheckedRepository extends JpaRepository<Checked, Integer> {
-    Checked getCheckedByAchiv_IdAndUserId(Integer achivId, Integer userId);
-}
